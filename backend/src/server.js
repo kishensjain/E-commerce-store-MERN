84c9bc06c8f186e2113c9832d638e3bc9ade3c24
Kishen S Jain <jain.skishen@gmail.com>
2026-06-15 10:58:32 +0530

    feat: add MongoDB connection setup and integrate with server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,23 @@
+// packages
 import express from "express";
 import dotenv from "dotenv";
+
+// routes/config
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
+connectDB();
+
 app.use(express.json());
 
 app.get("/", (_, res) => {
-    res.send("API is running");
+  res.send("API is running");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
