edd05e8077a052e3b7244e916000e0e6941f7a7c
Kishen S Jain <jain.skishen@gmail.com>
2026-06-18 10:28:09 +0530

    feat: add middleware to validate ObjectId format

---
+++ backend/src/middlewares/validateObjectId.middleware.js
@@ -0,0 +1,8 @@
+export const validateObjectId = (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({
+      message: "Invalid ID",
+    });
+  }
+  next();
+};
