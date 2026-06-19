9f76c7601da031b9d98ec4c4a7041df54f2f6e3e
Kishen S Jain <jain.skishen@gmail.com>
2026-06-19 10:43:35 +0530

    feat: Add order routes and place them in server.js

---
+++ backend/src/routes/order.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  placeOrder,
+  getOrders,
+  getMyOrders,
+  updateOrderStatus,
+  deleteOrder,
+  getOrderById,
+} from "../controllers/order.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
+import { admin } from "../middlewares/admin.middleware.js";
+
+const router = express.Router();
+
+router.route("/").post(protect, placeOrder).get(protect, admin, getOrders);
+router.route("/myorders").get(protect, getMyOrders);
+router.route("/:id/status").put(protect, admin, updateOrderStatus);
+router.route("/:id").get(protect, getOrderById).delete(protect, admin, deleteOrder);
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
+import orderRoutes from "./routes/order.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/", (_, res) => {
   res.send("API is running");
