d3b0d5effc110e63ffa56ed24be724005fd568f2
Kishen S Jain <jain.skishen@gmail.com>
2026-06-29 10:01:04 +0530

    feat: implement routing with MainLayout and AdminLayout components

---
+++ frontend/src/app/router.jsx
@@ -0,0 +1,68 @@
+import { createBrowserRouter } from "react-router";
+
+import AdminLayout from "@/layouts/AdminLayout";
+import MainLayout from "@/layouts/MainLayout";
+
+import DashboardPage from "@/pages/admin/DashboardPage";
+
+import LoginPage from "@/pages/auth/LoginPage";
+import RegisterPage from "@/pages/auth/RegisterPage";
+import VerifyOtpPage from "@/pages/auth/VerifyOtpPage";
+
+import CartPage from "@/pages/cart/CartPage";
+
+import OrdersPage from "@/pages/orders/OrdersPage";
+
+import ProfilePage from "@/pages/profile/ProfilePage";
+
+import HomePage from "@/pages/shop/HomePage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+
+      {
+        path: "verify-otp",
+        element: <VerifyOtpPage />,
+      },
+      {
+        path: "profile",
+        element: <ProfilePage />,
+      },
+
+      {
+        path: "orders",
+        element: <OrdersPage />,
+      },
+
+      {
+        path: "cart",
+        element: <CartPage />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <DashboardPage />,
+      },
+    ],
+  },
+]);
